6d3cdb1a1307648d86d106c59c00eb5eae5fe514
Timon <timon.borchers@googlemail.com>
2018-01-17 17:12:56 +0100

    init UI

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="shortcut icon" type="image/x-icon" href="/../images/favicon.ico">
+		<link href="css/custom/index.css" type="text/css" rel="stylesheet" />
+		<link href="css/generic/textbox.css" type="text/css" rel="stylesheet" />
+		<link href="css/generic/navbar.css" type="text/css" rel="stylesheet" />
+		<link href="css/generic/buttons.css" type="text/css" rel="stylesheet" />
+		<title>Tankstellen Preise</title>
+	</head>
+	<body>
+		<!--navbar -->
+		<header>
+			<div class="container">
+				<a href="index.php">
+				</a>
+				<nav>
+					<ul>
+						<li><a href="#">Login</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<!--main contents          -->
+		<form action="index.php" method="post">
+			<div id="main-area" class="container">
+				<div id="heading" class="page-header">
+					<h1>Suchen Sie die günstigste Tankstelle</h1>
+				</div>
+				<div id="griddiv-left" class="test">
+					<div id="rowend" class="row">
+						<input id="text-address" name="text-address" class="enjoy-css" type="text" placeholder="Ihr Standort"><br>
+					</div>
+					<div id="buttonrow" class="row">
+						<input class="button" type="submit" name="submit" value="Eingabe">
+					</div>
+				</div>
+				<div id="griddiv-right" class="test">
+					<div id="rowstart" class="row">
+						TODO: list
+
+						<br><br><br>
+					</div>
+					<div id="rowend" class="row">
+					</div>
+					<div id="resultstring" class="alert alert-info">
+					</div>
+				</div>
+			</div>
+		</form>
+	</body>
+</html>
