d88fc5414e7e0e9b770440d6fa4c38c7b35abafa
Timon <timon.borchers@googlemail.com>
2018-01-17 19:17:30 +0100

    change folder system

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="shortcut icon" type="image/x-icon" href="/../images/favicon.ico">
 		<link href="css/custom/index.css" type="text/css" rel="stylesheet" />
+		<link href="css/generic/body.css" type="text/css" rel="stylesheet" />
 		<link href="css/generic/textbox.css" type="text/css" rel="stylesheet" />
 		<link href="css/generic/navbar.css" type="text/css" rel="stylesheet" />
 		<link href="css/generic/buttons.css" type="text/css" rel="stylesheet" />
