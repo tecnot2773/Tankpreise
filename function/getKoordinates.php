6686eee6b146cc4509b75e39c68bea1304d404b3
Timon <timon.borchers@googlemail.com>
2018-01-24 12:07:28 +0100

    change path

--- function/getKoordinates.php
+++ function/getKoordinates.php
@@ -2,7 +2,7 @@
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		$address = $_POST["text-address"];
 
-		$http_content = file_get_contents("http://localhost/tankpreise/function/googleAPI.php" . "?address=$address");
+		$http_content = file_get_contents("http://tanken.criticalmission.de/tankpreise/function/googleAPI.php" . "?address=$address");
 
 		preg_match('/\[location] => stdClass Object\n *\(\n *\[lat] => (.+?)(?=\n)/' , $http_content, $latitude);
 		preg_match('/\[location] => stdClass Object\n *\(\n *\[lat] => \d*.\d*\n *\[lng] => (.+?)(?=\n)/', $http_content, $longitude);
