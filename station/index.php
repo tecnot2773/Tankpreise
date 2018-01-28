eb8a8b7ffdf8e8e9901f238cd4b679f25bb6775d
Timon <timon.borchers@googlemail.com>
2018-01-28 14:45:20 +0100

    Gasstation Page

---
+++ station/index.php
@@ -0,0 +1,13 @@
+<?php
+	include_once "../function/dbConnect.php";
+	include_once "statPrint.php";
+	$UUID = $_GET["id"];
+	$id = mysqli_fetch_array(mysqli_query($conn, "SELECT ID FROM gasstation WHERE UUID = '$UUID';"))['ID'];
+
+	echo "UUID: " . $UUID . "<br><br>";
+	statPrint("$id","diesel");
+	echo "<br>";
+	statPrint("$id","E5");
+	echo "<br>";
+	statPrint("$id","E10");
+ ?>
