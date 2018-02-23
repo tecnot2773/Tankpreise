8f5b2554716bc163db34096760a0987db89a1078
Timon <timon.borchers@googlemail.com>
2018-02-23 23:10:58 +0100

    init stats page

--- statistics.php
+++ statistics.php
@@ -1,1 +1,54 @@
-Nothing here yet
+<?php
+session_start();
+include_once "function/userConfig.php";
+if(!isset($_SESSION["address"]) || !isset($_SESSION["type"])){
+	getUserInfo();		//Get userinfo
+}
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="shortcut icon" type="image/x-icon" href="images/icon.jpg">
+		<link href="css/custom/index.css" type="text/css" rel="stylesheet" />
+		<link href="css/generic/body.css" type="text/css" rel="stylesheet" />
+		<link href="css/generic/textbox.css" type="text/css" rel="stylesheet" />
+		<link href="css/generic/navbar.css" type="text/css" rel="stylesheet" />
+		<link href="css/generic/buttons.css" type="text/css" rel="stylesheet" />
+		<link href="css/generic/icons.css" rel="stylesheet" />
+		<title>Tankstellen Preise</title>
+	</head>
+	<body>
+		<!--navbar -->
+		<header>
+			<div class="container">
+				<a href="index.php">
+					<img src="images/Header.png" alt="logo" class="logo" />
+				</a>
+				<nav>
+					<ul>
+						<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) { ?>
+						<li><a href="statistics.php">Statistik</a></li>
+						<li>
+							<div class="dropdown">
+								<a><?= $_SESSION['username'] ?></a>
+								<span class="icon icon-arrow-down2"></span>
+								<div class="dropdown-content">
+									<a href="user/index.php">Benutzer Seite</a>
+									<a href="user/userConfig.php">Einstellungen</a>
+									<a href="function/logout.php">Logout</a>
+								</div>
+							</div>
+						</li>
+					<?php  }else{ ?>
+						<li><a href="statistics.php">Statistik</a></li>
+						<li><a href="register.php">Registrieren</a></li>
+						<li><a href="login.php">Login</a></li>
+					<?php } ?>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<!--main contents          -->
