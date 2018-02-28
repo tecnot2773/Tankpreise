102bed84813669862bf38e1886bf5103099eb3a0
Timon <timon.borchers@googlemail.com>
2018-02-28 17:58:43 +0100

    preparations for overall stats

--- function/test.php
+++ function/test.php
@@ -1,16 +1,24 @@
 <?php
-$address = "blender";
-$url = 'https://maps.googleapis.com/maps/api/geocode/json'."?address=$address&apikey=8b284941-6a9c-30c6-1f12-9791a0b841dd";		//get koordinates via googleAPI
-$json = file_get_contents($url);
-$decoded = json_decode($json);
-print_r($decoded);
-echo "<br><br>";
-$land = $decoded->results[0]->address_components[3]->long_name;		//get longitude from array
-echo $decoded->status;
-if(isset($decoded->results[0]->address_components[2]->long_name)){$land = $decoded->results[0]->address_components[2]->long_name;}	//check if address is in Germany
-if(isset($decoded->results[0]->address_components[3]->long_name) && $land != "Germany"){$land = $decoded->results[0]->address_components[3]->long_name;}
-if(isset($decoded->results[0]->address_components[4]->long_name) && $land != "Germany"){$land = $decoded->results[0]->address_components[4]->long_name;}
-echo $land;
+include_once "dbConnect.php";
+$query = "SELECT avg(diesel) AS diesel FROM stats WHERE MONTH(timestamp) = ? AND DAY(timestamp) = ? AND YEAR(timestamp) = ? AND HOUR(timestamp) = ?;";
+if ($stmt = $mysqli->prepare($query)) {		//prepare statement to get stats
+	$stmt->bind_param("ssss", $month, $day, $year, $hour);		//bind parameters
+	$day = date("d", strtotime("last Monday"));		//day from last monday
+	$month = date("m", strtotime("last Monday"));	//month from last monday
+	$year = date("Y", strtotime("last Monday"));	//year from last monday
+	for($i = 0; $i < 24; $i++){
+		$hour = $i;
+		//echo "SELECT avg(diesel) AS diesel FROM stats WHERE MONTH(timestamp) = $month AND DAY(timestamp) = $day AND YEAR(timestamp) = $year AND HOUR(timestamp) = $hour;";
+		$stmt->execute();			//execute statement
+		$result = $stmt->get_result();		//save result
+		 while($data = $result->fetch_array()){
+			 $diesel = $data["diesel"];
+
+			 echo $diesel . "<br>";
+		}
+	}
+}
+
 /*
 https://stackoverflow.com/questions/8994718/mysql-longitude-and-latitude-query-for-other-rows-within-x-mile-radius
 SELECT radius
