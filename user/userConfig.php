5febb66b80c39d8c9d56ec1dfaa5baed5b74c386
tecnot <tecnot2773@users.noreply.github.com>
2018-03-08 10:29:36 +0100

    Update userConfig.php

--- user/userConfig.php
+++ user/userConfig.php
@@ -7,6 +7,9 @@
 	if(!isset($_SESSION["address"]) && $_SESSION['loggedin'] == true || !isset($_SESSION["type"]) && $_SESSION['loggedin'] == true){
 		getUserInfo();
 	}
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	     getUserInfo();
+	}
 ?>
 <!DOCTYPE html>
 <html lang="en">
